e1f43e06c148ed3f48bc14c58f97a14b1e48d54d
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-02-19 12:01:49 +0900

    ファイル形式をJSONに変更し、ファイル名をidに、内容をメモのタイトルと本文に変更。

--- memo.rb
+++ memo.rb
@@ -2,42 +2,25 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-
-enable :method_override
+require 'json'
+require 'securerandom'
 
 def h(text)
   Rack::Utils.escape_html(text)
 end
 
-def fname_to_id(fname)
-  %r{^./memos/(\d)+_}.match(fname)
+def get_contains(id)
+  JSON.parse(File.open("./memos/#{id}.json").read)
 end
 
-def id_to_title(id)
-  memo_path = Dir.glob("./memos/#{id}_*").to_s
-  File.basename(memo_path, '.*').split(/^\d+_/)[1]
-end
-
-def write_body(title, body)
-  open("./memos/#{title}.txt", 'w') { |f| f.puts body }
-end
-
-def make_path(id, title)
-  "./memos/#{id}_#{title}.txt"
-end
-
-def id_formatter(id)
-  format('%03d', id)
+def write_memo(id, title, body)
+  File.open("./memos/#{id}.json", 'w') do |file|
+    hash = { 'title' => title.to_s, 'body' => body.to_s }
+    JSON.dump(hash, file)
+  end
 end
 
 Dir.mkdir('memos') unless Dir.exist?('./memos')
-
-id = if Dir.empty?('./memos')
-       0
-     else
-       last_name = Dir.glob('./memos/*').last
-       fname_to_id(last_name)[1].to_i
-     end
 
 get '/' do
   redirect to('/memos')
@@ -45,7 +28,10 @@ end
 
 get '/memos' do
   @page_title = 'top'
-  @titles = Dir.glob('./memos/*').map { |file| File.basename(file, '.*') }
+
+  files = Dir.glob('./memos/*')
+  files.sort_by! { |file| File.new(file).birthtime }
+  @ids = files.map { |file| File.basename(file, '.*') }
 
   erb :top
 end
@@ -57,30 +43,29 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  @title = params[:title].tr('/', '-')
-  @body = params[:body]
-  id += 1
-  format_id = id_formatter(id)
+  title = params[:title]
+  body = params[:body]
+  id = SecureRandom.uuid
 
   redirect to('/no_title_error') if params[:title].empty?
 
-  write_body("#{format_id}_#{@title}", @body)
+  write_memo(id, title, body)
 
   redirect to('/')
 end
 
 get '/memos/*/' do |id|
   @id = id
-  @title = h(id_to_title(id))
-  @body = h(File.open(make_path(id, @title), &:read)).gsub("\r\n", '<br>')
+  @title = h(get_contains(id)['title'])
+  @body = h(get_contains(id)['body']).gsub("\r\n", '<br>')
   @page_title = @title
 
   erb :memo_template
 end
 
 delete '/memos/*/delete' do |id|
-  title = id_to_title(id)
-  File.delete(make_path(id, title))
+  path = "./memos/#{id}.json"
+  File.delete(path)
 
   redirect to('/')
 end
@@ -88,21 +73,19 @@ end
 get '/memos/*/edit' do |id|
   @page_title = 'メモを編集'
   @id = id
-  @title = id_to_title(id)
-  @body = File.open(make_path(id, @title), &:read)
+  @title = get_contains(id)['title']
+  @body = get_contains(id)['body']
 
   erb :memo_edit
 end
 
 patch '/memos/*/update' do |id|
-  @new_title = params[:title]
-  @new_body = params[:body]
-  old_title = id_to_title(id)
+  new_title = params[:title]
+  new_body = params[:body]
 
   redirect to('/no_title_error') if params[:title].empty?
 
-  write_body("#{id}_#{old_title}", @new_body)
-  File.rename(make_path(id, old_title), make_path(id, @new_title))
+  write_memo(id, new_title, new_body)
 
   redirect to('/')
 end
