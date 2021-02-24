17c7310e8593a73291f6958b0c5fc753c7c0ad69
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-02-24 14:53:10 +0900

    メソッドをリファクタリングし、不要なインスタンス変数を変数に変更

--- memo.rb
+++ memo.rb
@@ -9,7 +9,7 @@ enable :method_override
 
 CONN = PG.connect(dbname: 'memoapp')
 
-create_str = 'CREATE TABLE if not exists memolist (id text not null unique, title text not null unique, body text)'
+create_str = 'CREATE TABLE if not exists memolist (id text not null unique, title text not null, body text)'
 CONN.exec(create_str)
 
 add_str = 'INSERT into memolist (id,title,body) values ($1,$2,$3)'
@@ -18,22 +18,19 @@ CONN.prepare('add_memo', add_str)
 update_str = 'UPDATE memolist set title=$1, body=$2 where id=$3'
 CONN.prepare('update_memo', update_str)
 
-get_title_str = 'SELECT title from memolist where id=$1'
-CONN.prepare('get_title', get_title_str)
-
-get_body_str = "SELECT body from memolist where id=$1"
-CONN.prepare('get_body', get_body_str)
+get_memo_str = 'SELECT * from memolist where id=$1'
+CONN.prepare('get_memo', get_memo_str)
 
 def h(text)
   Rack::Utils.escape_html(text)
 end
 
 def get_body(id)
-  CONN.exec_prepared('get_body', [id]).values[0][0]
+  CONN.exec_prepared('get_memo', [id])[0]['body']
 end
 
 def get_title(id)
-  CONN.exec_prepared('get_title', [id]).values[0][0]
+  CONN.exec_prepared('get_memo', [id])[0]['title']
 end
 
 get '/' do
@@ -51,28 +48,27 @@ get '/memos' do
   erb :top
 end
 
-get '/new' do
+get '/memos/new' do
   @page_title = 'メモを新規作成'
 
   erb :new_memo
 end
 
-post '/create' do
-  @title = params[:title]
-  @body = params[:body]
+post '/memos' do
+  title = params[:title]
+  body = params[:body]
   id = SecureRandom.uuid
 
   redirect to('/no_title_error') if params[:title].empty?
 
-  CONN.exec_prepared('add_memo', [id, @title.to_s, @body.to_s])
+  CONN.exec_prepared('add_memo', [id, title, body])
 
   redirect to('/')
 end
 
 get '/memos/:id/' do |id|
   @id = id
-  @title = h(get_title(id))
-  @page_title = @title
+  @title = @page_title = h(get_title(id))
   @body = h(get_body(id)).gsub(/\R/, '<br>')
 
   erb :memo_template
@@ -95,12 +91,12 @@ get '/memos/:id/edit' do |id|
 end
 
 patch '/memos/:id/update' do |id|
-  @new_title = params[:title]
-  @new_body = params[:body]
+  new_title = params[:title]
+  new_body = params[:body]
 
   redirect to('/no_title_error') if params[:title].empty?
 
-  CONN.exec_prepared('update_memo', [@new_title, @new_body, id])
+  CONN.exec_prepared('update_memo', [new_title, new_body, id])
 
   redirect to('/')
 end
